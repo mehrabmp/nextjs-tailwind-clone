b4f24e372e52db164c408a97d7c5efd706bacb98
Mehrabmp <mehrabmpdev@gmail.com>
2022-12-04 00:18:38 +0330

    refactor: create layout and page in app dir

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter, Space_Grotesk } from '@next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-grotesk',
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
