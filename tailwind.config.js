2567ab142db7aa8f7d8f8e921737352778c43bc1
Mehrabmp <mehrabmpdev@gmail.com>
2022-12-04 00:23:26 +0330

    chore: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} \*/
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -14,6 +15,9 @@ module.exports = {
       },
       colors: {
         primary: '#0070f3',
+      },
+      boxShadow: {
+        card: '0 0 0 1px rgb(0 0 0 / 7%), 0 2px 4px rgb(0 0 0 / 5%), 0 12px 24px rgb(0 0 0 / 5%)',
       },
     },
   },
